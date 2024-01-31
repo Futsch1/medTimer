381483c96fbd5204edac94431d786e88fe98c4a6
Futsch1 <florian.fetz@googlemail.com>
2024-01-31 19:23:02 +0100

    Fixed autostart

--- app/src/main/java/com/futsch1/medtimer/Autostart.java
+++ app/src/main/java/com/futsch1/medtimer/Autostart.java
@@ -6,10 +6,10 @@ import android.content.Intent;
 import android.util.Log;
 
 public class Autostart extends BroadcastReceiver {
-    public void onReceive(Context context, Intent arg1) {
-        Intent intent = new Intent(context, ReminderSchedulerService.class);
+    public void onReceive(Context context, Intent intent) {
         if (intent.getAction() != null && intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            context.startForegroundService(intent);
+            Intent serviceIntent = new Intent(context, ReminderSchedulerService.class);
+            context.startForegroundService(serviceIntent);
             Log.i("Service", "Started Reminder  SchedulerService");
         }
     }
