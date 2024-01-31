32ce1e7871c1b0511aae5f00c908b84fcb4a7ab5
Futsch1 <florian.fetz@googlemail.com>
2024-01-31 19:38:20 +0100

    Bumped version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.futsch1.medtimer"
         minSdk = 33
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
