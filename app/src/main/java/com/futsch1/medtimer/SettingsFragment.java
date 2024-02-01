3cffd813a9e4441c031777a65474defc1a92ee58
Futsch1 <florian.fetz@googlemail.com>
2024-02-01 17:17:36 +0100

    Show version in settings

--- app/src/main/java/com/futsch1/medtimer/SettingsFragment.java
+++ app/src/main/java/com/futsch1/medtimer/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.futsch1.medtimer;
 
 import android.os.Bundle;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -9,5 +10,10 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        Preference preference = getPreferenceScreen().findPreference("version");
+        if (preference != null) {
+            preference.setTitle(getString(R.string.version, BuildConfig.VERSION_NAME));
+        }
     }
 }
