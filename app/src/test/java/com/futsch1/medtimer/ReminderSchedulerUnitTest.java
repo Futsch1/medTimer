4537f381dbf08b3a37cdd227e61647a732bc02d4
Futsch1 <florian.fetz@googlemail.com>
2024-02-18 08:31:39 +0100

    Removed example comment

--- app/src/test/java/com/futsch1/medtimer/ReminderSchedulerUnitTest.java
+++ app/src/test/java/com/futsch1/medtimer/ReminderSchedulerUnitTest.java
@@ -20,11 +20,6 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ReminderSchedulerUnitTest {
 
     @Test
