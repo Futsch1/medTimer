4c267ceba247963edd595f162c940319586e7a99
Florian Fetz <63090558+Futsch1@users.noreply.github.com>
2024-02-24 17:16:18 +0100

    Medicine background (#8)
    * Optimized display of cards in Android 13
    
    * Bumped version to v1.0.2

--- app/src/main/java/com/futsch1/medtimer/helpers/ViewColorHelper.java
+++ app/src/main/java/com/futsch1/medtimer/helpers/ViewColorHelper.java
@@ -1,7 +1,5 @@
 package com.futsch1.medtimer.helpers;
 
-import android.content.Context;
-import android.util.TypedValue;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -9,6 +7,8 @@ import androidx.annotation.ColorInt;
 import androidx.core.graphics.ColorUtils;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.color.MaterialColors;
+import com.google.android.material.elevation.SurfaceColors;
 
 import java.util.List;
 
@@ -19,18 +19,13 @@ public class ViewColorHelper {
     }
 
     public static void setCardBackground(MaterialCardView cardView, List<TextView> textViews, @ColorInt int backgroundColor) {
-        int defaultTextViewColor = getThemeColor(cardView.getContext(), com.google.android.material.R.attr.colorOnSurface);
+        int defaultTextViewColor = MaterialColors.getColor(cardView, com.google.android.material.R.attr.colorOnSurface);
         double contrastTextView = ColorUtils.calculateContrast(defaultTextViewColor, backgroundColor | 0xFF000000);
-        int cardDefaultBackground = getThemeColor(cardView.getContext(), com.google.android.material.R.attr.colorSurface);
+        int cardDefaultBackground = SurfaceColors.getColorForElevation(cardView.getContext(), cardView.getElevation());
         double contrastBackground = ColorUtils.calculateContrast(cardDefaultBackground, backgroundColor | 0xFF000000);
+
         setTextColor(textViews, contrastTextView < contrastBackground ? cardDefaultBackground : defaultTextViewColor);
         cardView.setCardBackgroundColor(backgroundColor);
-    }
-
-    private static int getThemeColor(Context context, int attribute) {
-        TypedValue themeColor = new TypedValue();
-        context.getTheme().resolveAttribute(attribute, themeColor, true);
-        return themeColor.data;
     }
 
     private static void setTextColor(List<TextView> textViews, @ColorInt int color) {
@@ -40,8 +35,8 @@ public class ViewColorHelper {
     }
 
     public static void setButtonBackground(Button button, @ColorInt int backgroundColor) {
-        int primaryColor = getThemeColor(button.getContext(), com.google.android.material.R.attr.colorPrimary);
-        int onPrimaryColor = getThemeColor(button.getContext(), com.google.android.material.R.attr.colorOnPrimary);
+        int primaryColor = MaterialColors.getColor(button, com.google.android.material.R.attr.colorOnSurface);
+        int onPrimaryColor = MaterialColors.getColor(button, com.google.android.material.R.attr.colorOnPrimary);
         double primaryContrast = ColorUtils.calculateContrast(primaryColor, backgroundColor | 0xFF000000);
         double onPrimaryContrast = ColorUtils.calculateContrast(onPrimaryColor, backgroundColor | 0xFF000000);
         setTextColor(button, primaryContrast > onPrimaryContrast ? primaryColor : onPrimaryColor);
@@ -53,8 +48,9 @@ public class ViewColorHelper {
     }
 
     public static void setDefaultColors(MaterialCardView cardView, List<TextView> textViews) {
-        int defaultTextViewColor = getThemeColor(cardView.getContext(), com.google.android.material.R.attr.colorOnSurfaceVariant);
-        int cardDefaultBackground = getThemeColor(cardView.getContext(), com.google.android.material.R.attr.colorSurface);
+        int defaultTextViewColor = MaterialColors.getColor(cardView, com.google.android.material.R.attr.colorOnSurface);
+        int cardDefaultBackground = SurfaceColors.getColorForElevation(cardView.getContext(), cardView.getElevation());
+
         cardView.setCardBackgroundColor(cardDefaultBackground);
         setTextColor(textViews, defaultTextViewColor);
     }
