76bdc4aca59bb21886ffc8d98fb8511221e5e205
Futsch1 <florian.fetz@googlemail.com>
2024-04-06 11:52:09 +0200

    Implemented weekend mode

--- app/src/main/java/com/futsch1/medtimer/WeekendModePreferencesFragment.java
+++ app/src/main/java/com/futsch1/medtimer/WeekendModePreferencesFragment.java
@@ -8,6 +8,7 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceManager;
 
 import com.futsch1.medtimer.helpers.TimeHelper;
+import com.futsch1.medtimer.reminders.ReminderProcessor;
 import com.takisoft.preferencex.PreferenceFragmentCompat;
 
 import java.util.List;
@@ -16,13 +17,26 @@ import java.util.stream.Collectors;
 public class WeekendModePreferencesFragment extends PreferenceFragmentCompat {
 
     public static final String WEEKEND_TIME = "weekend_time";
+    public static final String WEEKEND_MODE = "weekend_mode";
+    public static final String WEEKEND_DAYS = "weekend_days";
 
     @Override
     public void onCreatePreferencesFix(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.weekend_mode_preferences, rootKey);
 
+        setupWeekendMode();
         setupTimePicker();
         setupDays();
+    }
+
+    private void setupWeekendMode() {
+        Preference preference = getPreferenceScreen().findPreference(WEEKEND_MODE);
+        if (preference != null) {
+            preference.setOnPreferenceChangeListener((preference1, newValue) -> {
+                requestReschedule();
+                return true;
+            });
+        }
     }
 
     private void setupTimePicker() {
@@ -35,6 +49,7 @@ public class WeekendModePreferencesFragment extends PreferenceFragmentCompat {
                 new TimeHelper.TimePickerWrapper(requireContext()).show(weekendTime / 60, weekendTime % 60, minutes -> {
                     defaultSharedPreferences.edit().putInt(WEEKEND_TIME, minutes).apply();
                     preference1.setSummary(TimeHelper.minutesToTime(minutes));
+                    requestReschedule();
                 });
                 return true;
             });
@@ -42,13 +57,21 @@ public class WeekendModePreferencesFragment extends PreferenceFragmentCompat {
     }
 
     private void setupDays() {
-        Preference preference = getPreferenceScreen().findPreference("weekend_days");
+        Preference preference = getPreferenceScreen().findPreference(WEEKEND_DAYS);
         if (preference != null) {
             preference.setSummaryProvider((Preference.SummaryProvider<MultiSelectListPreference>) preference1 -> {
                 @SuppressWarnings("java:S6204") // Using SDK 33
                 List<CharSequence> values = preference1.getValues().stream().map(s -> preference1.getEntries()[Integer.parseInt(s) - 1]).collect(Collectors.toList());
                 return String.join(", ", values);
             });
+            preference.setOnPreferenceChangeListener((preference12, newValue) -> {
+                requestReschedule();
+                return true;
+            });
         }
+    }
+
+    private void requestReschedule() {
+        ReminderProcessor.requestReschedule(requireContext());
     }
 }
---
+++ app/src/main/java/com/futsch1/medtimer/reminders/WeekendMode.java
@@ -0,0 +1,40 @@
+package com.futsch1.medtimer.reminders;
+
+import android.content.SharedPreferences;
+import android.util.ArraySet;
+
+import com.futsch1.medtimer.WeekendModePreferencesFragment;
+
+import java.time.DayOfWeek;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+public class WeekendMode {
+
+    private final SharedPreferences preferences;
+
+    public WeekendMode(SharedPreferences preferences) {
+        this.preferences = preferences;
+    }
+
+    public Instant adjustInstant(Instant instant) {
+        if (isWeekendModeEnabled()) {
+            int weekendTime = preferences.getInt(WeekendModePreferencesFragment.WEEKEND_TIME, 540);
+            Set<String> weekendDays = preferences.getStringSet(WeekendModePreferencesFragment.WEEKEND_DAYS, new ArraySet<>());
+            ZonedDateTime localDateTime = instant.atZone(ZoneId.systemDefault());
+            DayOfWeek dayOfWeek = localDateTime.getDayOfWeek();
+            int minutes = localDateTime.getMinute() + localDateTime.getHour() * 60;
+            int deltaMinutes = weekendTime - minutes;
+            if (weekendDays.contains(String.valueOf(dayOfWeek.getValue())) && deltaMinutes > 0) {
+                instant = instant.plusSeconds(deltaMinutes * 60L);
+            }
+        }
+        return instant;
+    }
+
+    private boolean isWeekendModeEnabled() {
+        return preferences.getBoolean(WeekendModePreferencesFragment.WEEKEND_MODE, false);
+    }
+}
