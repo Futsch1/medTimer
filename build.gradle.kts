a7b7c37c663d628706e01c6678d3a3df3de26dfd
Futsch1 <florian.fetz@googlemail.com>
2024-05-05 13:02:04 +0200

    Updated to AGP 8.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.3.2" apply false
+    id("com.android.application") version "8.4.0" apply false
     id("androidx.room") version "2.6.1" apply false
     id("androidx.navigation.safeargs") version "2.7.7" apply false
 }
