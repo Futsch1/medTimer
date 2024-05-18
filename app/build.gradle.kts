9cf0bc45faea422b1a0aa9e23b23d62a7b5c609a
Futsch1 <florian.fetz@googlemail.com>
2024-05-18 09:24:05 +0200

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.work:work-runtime:2.9.0")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.tracing:tracing:1.2.0")
     implementation("org.mockito:mockito-core:5.12.0")
@@ -86,12 +86,12 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-inline:5.2.0")
-    testImplementation("org.robolectric:robolectric:4.12.1")
+    testImplementation("org.robolectric:robolectric:4.12.2")
 
-    androidTestImplementation("androidx.test.ext:junit:1.2.0-alpha04")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0-alpha04")
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.6.0-alpha04")
-    androidTestImplementation("androidx.test:rules:1.6.0-alpha04")
+    androidTestImplementation("androidx.test.ext:junit:1.2.0-beta01")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0-beta01")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.6.0-beta01")
+    androidTestImplementation("androidx.test:rules:1.6.0-beta01")
 
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.8.1")
