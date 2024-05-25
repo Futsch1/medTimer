595d1806b1430fa993d54b8c14f45e7d0e8b6490
Futsch1 <florian.fetz@googlemail.com>
2024-05-25 13:16:39 +0200

    Implemented taken or skipped now

--- app/src/main/java/com/futsch1/medtimer/overview/NextRemindersViewHolder.java
+++ app/src/main/java/com/futsch1/medtimer/overview/NextRemindersViewHolder.java
@@ -1,11 +1,12 @@
 package com.futsch1.medtimer.overview;
 
+import static android.text.format.DateUtils.isToday;
+
 import android.os.Handler;
 import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -18,6 +19,7 @@ import com.futsch1.medtimer.helpers.ViewColorHelper;
 import com.futsch1.medtimer.reminders.ReminderProcessor;
 import com.futsch1.medtimer.reminders.ReminderWork;
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.chip.Chip;
 
 import java.time.Instant;
 import java.time.ZoneId;
@@ -41,14 +43,19 @@ public class NextRemindersViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(ScheduledReminder scheduledReminder, Looper looper, MedicineViewModel medicineViewModel) {
-        Button takenNow = itemView.findViewById(R.id.takenNow);
-        Button skippedNow = itemView.findViewById(R.id.skippedNow);
+        Chip takenNow = itemView.findViewById(R.id.takenNow);
+        Chip skippedNow = itemView.findViewById(R.id.skippedNow);
         takenNow.setOnClickListener(v -> processFutureReminder(scheduledReminder, true, looper, medicineViewModel));
         skippedNow.setOnClickListener(v -> processFutureReminder(scheduledReminder, false, looper, medicineViewModel));
 
         ZonedDateTime reminderTime = scheduledReminder.timestamp().atZone(ZoneId.systemDefault());
         String nextTime = reminderTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT));
         nextReminderText.setText(nextReminderText.getContext().getString(R.string.reminder_event, scheduledReminder.reminder().amount, scheduledReminder.medicine().name, nextTime));
+
+        boolean isToday = isToday(scheduledReminder.timestamp().toEpochMilli());
+        takenNow.setVisibility(isToday ? View.VISIBLE : View.GONE);
+        skippedNow.setVisibility(isToday ? View.VISIBLE : View.GONE);
+
         if (scheduledReminder.medicine().useColor) {
             ViewColorHelper.setCardBackground((MaterialCardView) itemView, Collections.singletonList(nextReminderText), scheduledReminder.medicine().color);
         } else {
