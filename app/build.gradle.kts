b91dc527b84b379526b7457cc9c6a4202e1d721b
Florian Fetz <63090558+Futsch1@users.noreply.github.com>
2024-05-29 18:41:32 +0200

    Next reminders fixes (#135)
    * Hide expand button for empty next reminders list
    Scroll to top when changing next reminders
    
    * Firebase tests (#134)
    
    * Using explicit gcloud interface for tests
    Added robo test
    
    * Retries for critical, not working part
    
    * Bumped version to v1.8.6
    
    * Fixed GCloud actions
    
    * Fixed Firebase workflow
    
    * Removed the pull request option from the workflow
    
    * Increased test timeout
    
    * Added tests for timezone handling in scheduler
    
    * Fixed retrying
    
    * Testing ReminderWork
    
    * Testing actual reminder work behavior

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.futsch1.medtimer"
         minSdk = 33
         targetSdk = 34
-        versionCode = 50
-        versionName = "1.8.5"
+        versionCode = 51
+        versionName = "1.8.6"
         setProperty("archivesBaseName", "MedTimer")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
