9f339079bdd81da6c5535e5843eb775fc224f226
Futsch1 <florian.fetz@googlemail.com>
2024-07-13 10:07:34 +0200

    Bumped version to v1.8.13-rc.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.futsch1.medtimer"
         minSdk = 33
         targetSdk = 34
-        versionCode = 58
-        versionName = "1.8.12"
+        versionCode = 59
+        versionName = "1.8.13-rc.1"
         setProperty("archivesBaseName", "MedTimer")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
