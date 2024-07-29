b07db34bc8290933aea747d54c519def49f1f7c3
Futsch1 <florian.fetz@googlemail.com>
2024-07-29 17:30:30 +0200

    Removed duplicated code

--- app/src/main/java/com/futsch1/medtimer/reminders/SkippedWork.java
+++ app/src/main/java/com/futsch1/medtimer/reminders/SkippedWork.java
@@ -1,25 +1,14 @@
 package com.futsch1.medtimer.reminders;
 
-import static com.futsch1.medtimer.ActivityCodes.EXTRA_REMINDER_EVENT_ID;
-
 import android.content.Context;
 
 import androidx.annotation.NonNull;
-import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 import com.futsch1.medtimer.database.ReminderEvent;
 
-public class SkippedWork extends Worker {
+public class SkippedWork extends NotificationWork {
     public SkippedWork(@NonNull Context context, @NonNull WorkerParameters workerParams) {
-        super(context, workerParams);
-    }
-
-    @NonNull
-    @Override
-    public Result doWork() {
-        NotificationAction.processNotification(getApplicationContext(), getInputData().getInt(EXTRA_REMINDER_EVENT_ID, 0), ReminderEvent.ReminderStatus.SKIPPED);
-
-        return Result.success();
+        super(context, workerParams, ReminderEvent.ReminderStatus.SKIPPED);
     }
 }
