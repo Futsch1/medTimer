fe6be216af8a7600dab8fc28f17fd3436e300a4d
Futsch1 <florian.fetz@googlemail.com>
2024-09-11 22:16:26 +0200

    Translate can now also re-translate strings

--- translate.py
+++ translate.py
@@ -19,6 +19,11 @@ for string_name in strings:
         continue
     english_strings[string_name.attrib['name']] = string_name.text
 
+if len(sys.argv) > 2:
+    translate_args = sys.argv[2:]
+else:
+    translate_args = []
+
 # Search for strings in other languages and build the list of strings to be translated
 for directory in glob.glob('app/src/main/res/*'):
     if directory == 'app/src/main/res/values' or not os.path.exists(directory + '/strings.xml'):
@@ -30,7 +35,7 @@ for directory in glob.glob('app/src/main/res/*'):
     for string_name in resources.findall('string'):
         language_strings.append(string_name.attrib['name'])
     for english_string_name in english_strings.keys():
-        if english_string_name not in language_strings:
+        if english_string_name not in language_strings or not language_strings[language_strings.index(english_string_name)] or english_string_name in translate_args:
             translate_list.append((english_string_name, language))
 auth_key = sys.argv[1]
 
@@ -38,8 +43,11 @@ auth_key = sys.argv[1]
 new_elements = {}
 translator = deepl.Translator(auth_key)
 for string_name, language in translate_list:
+    print('Translating ' + string_name + ' to ' + language)
     translated_string = translator.translate_text(english_strings[string_name], target_lang=language, preserve_formatting=True)
-    new_elements[language] = ET.SubElement(language_tree[language].getroot(), 'string', name=string_name)
+    new_elements[language] = language_tree[language].find('string[@name="' + string_name + '"]')
+    if new_elements[language] is None:
+        new_elements[language] = ET.SubElement(language_tree[language].getroot(), 'string', name=string_name)
     new_elements[language].text = translated_string.text
 
 # Collect all languages where a translation string was found
