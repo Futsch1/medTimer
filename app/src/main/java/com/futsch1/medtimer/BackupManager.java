c7a97138a77c37407ac1efa12ba936a10db64bad
Futsch1 <florian.fetz@googlemail.com>
2024-09-11 22:14:53 +0200

    Implemented combined backup

--- app/src/main/java/com/futsch1/medtimer/BackupManager.java
+++ app/src/main/java/com/futsch1/medtimer/BackupManager.java
@@ -17,9 +17,15 @@ import com.futsch1.medtimer.database.JSONMedicineBackup;
 import com.futsch1.medtimer.database.JSONReminderEventBackup;
 import com.futsch1.medtimer.helpers.FileHelper;
 import com.futsch1.medtimer.helpers.PathHelper;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.File;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BackupManager {
     private final Context context;
@@ -27,7 +33,9 @@ public class BackupManager {
     private final Menu menu;
     private final MedicineViewModel medicineViewModel;
     private final ActivityResultLauncher<Intent> openFileLauncher;
-    private PendingFileOperation pendingFileOperation = PendingFileOperation.NONE;
+
+    private final String medicineKey = "medicines";
+    private final String eventKey = "events";
 
 
     public BackupManager(Context context, Menu menu, MedicineViewModel medicineViewModel, ActivityResultLauncher<Intent> openFileLauncher) {
@@ -39,76 +47,95 @@ public class BackupManager {
         backgroundThread.start();
 
         setupBackup();
-        setupEventsBackup();
     }
 
     private void setupBackup() {
-        Handler handler = new Handler(backgroundThread.getLooper());
-
-        MenuItem item = menu.findItem(R.id.medicine_backup);
+        MenuItem item = menu.findItem(R.id.create_backup);
         item.setOnMenuItemClickListener(menuItem -> {
-            handler.post(() ->
-                    createBackup(new JSONMedicineBackup(),
-                            medicineViewModel.medicineRepository.getMedicines(),
-                            "Medicine"));
+            selectBackupType();
             return true;
         });
 
-        item = menu.findItem(R.id.restore_medicine_backup);
+        item = menu.findItem(R.id.restore_backup);
         item.setOnMenuItemClickListener(menuItem -> {
             new AlertDialog.Builder(context)
                     .setTitle(R.string.restore)
                     .setMessage(R.string.restore_start)
                     .setCancelable(true)
-                    .setPositiveButton(R.string.ok, (dialog, which) -> openBackup(PendingFileOperation.MEDICINE))
+                    .setPositiveButton(R.string.ok, (dialog, which) -> openBackup())
                     .setNegativeButton(R.string.cancel, (dialog, which) -> { // Intentionally left empty
                     }).show();
             return true;
         });
     }
 
-    private void setupEventsBackup() {
-        Handler handler = new Handler(backgroundThread.getLooper());
-
-        MenuItem item = menu.findItem(R.id.backup_events);
-        item.setOnMenuItemClickListener(menuItem -> {
-            handler.post(() ->
-                    createBackup(new JSONReminderEventBackup(),
-                            medicineViewModel.medicineRepository.getAllReminderEventsWithoutDeleted(),
-                            "ReminderEvents"));
-            return true;
+    private void selectBackupType() {
+        MaterialAlertDialogBuilder alertDialogBuilder = new MaterialAlertDialogBuilder(context);
+        alertDialogBuilder.setTitle(R.string.backup);
+        boolean[] checkedItems = new boolean[2];
+        alertDialogBuilder.setMultiChoiceItems(R.array.backup_types, null, (dialog, which, isChecked) -> checkedItems[which] = isChecked);
+        alertDialogBuilder.setPositiveButton(R.string.ok, (dialog, which) -> {
+            Handler handler = new Handler(backgroundThread.getLooper());
+            handler.post(() -> {
+                Gson gson = new Gson();
+                Map<String, String> backupMap = new LinkedHashMap<>();
+                if (checkedItems[0]) {
+                    backupMap.put(medicineKey, createBackup(new JSONMedicineBackup(),
+                            medicineViewModel.medicineRepository.getMedicines()));
+                }
+                if (checkedItems[1]) {
+                    backupMap.put(eventKey, createBackup(new JSONReminderEventBackup(),
+                            medicineViewModel.medicineRepository.getAllReminderEventsWithoutDeleted()));
+                }
+                createAndSave(gson.toJson(backupMap));
+            });
         });
-
-        item = menu.findItem(R.id.restore_events);
-        item.setOnMenuItemClickListener(menuItem -> {
-            new AlertDialog.Builder(context)
-                    .setTitle(R.string.restore)
-                    .setMessage(R.string.restore_events_start)
-                    .setCancelable(true)
-                    .setPositiveButton(R.string.ok, (dialog, which) -> openBackup(PendingFileOperation.REMINDER_EVENTS))
-                    .setNegativeButton(R.string.cancel, (dialog, which) -> { // Intentionally left empty
-                    }).show();
-            return true;
-        });
+        alertDialogBuilder.show();
     }
 
-    private <T> void createBackup(JSONBackup<T> jsonBackup, List<T> backupData, String backupType) {
-        String jsonContent = jsonBackup.createBackup(medicineViewModel.medicineRepository.getVersion(),
+    private void openBackup() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("application/json");
+        openFileLauncher.launch(intent);
+    }
+
+    private <T> String createBackup(JSONBackup<T> jsonBackup, List<T> backupData) {
+        return jsonBackup.createBackup(medicineViewModel.medicineRepository.getVersion(),
                 backupData);
-        createAndSave(jsonContent, backupType);
+    }
+
+    private void createAndSave(String fileContent) {
+        File file = new File(context.getCacheDir(), PathHelper.getBackupFilename());
+        if (FileHelper.saveToFile(file, fileContent)) {
+            FileHelper.shareFile(context, file);
+        } else {
+            Toast.makeText(context, R.string.backup_failed, Toast.LENGTH_LONG).show();
+        }
     }
 
     public void fileSelected(Uri data) {
         String json = FileHelper.readFromUri(data, context.getContentResolver());
         boolean restoreSuccessful = false;
         if (json != null) {
-            if (pendingFileOperation == PendingFileOperation.MEDICINE) {
-                restoreSuccessful = restoreBackup(json, new JSONMedicineBackup());
-            } else if (pendingFileOperation == PendingFileOperation.REMINDER_EVENTS) {
-                restoreSuccessful = restoreBackup(json, new JSONReminderEventBackup());
+            TypeToken<Map<String, String>> mapType = new TypeToken<>() {
+            };
+            try {
+                Map<String, String> backupMap = new Gson().fromJson(json, mapType.getType());
+                if (backupMap.containsKey(medicineKey)) {
+                    restoreSuccessful = restoreBackup(backupMap.get(medicineKey), new JSONMedicineBackup());
+                }
+                if (backupMap.containsKey(eventKey)) {
+                    restoreSuccessful = restoreSuccessful && restoreBackup(backupMap.get(eventKey), new JSONReminderEventBackup());
+                }
+            } catch (JsonSyntaxException e) {
+                restoreSuccessful = false;
             }
+        }
 
-            pendingFileOperation = PendingFileOperation.NONE;
+        if (!restoreSuccessful) {
+            // Try legacy backup formats
+            restoreSuccessful = restoreBackup(json, new JSONMedicineBackup()) || restoreBackup(json, new JSONReminderEventBackup());
         }
 
         new AlertDialog.Builder(context)
@@ -126,23 +153,6 @@ public class BackupManager {
             return true;
         }
         return false;
-    }
-
-    private void createAndSave(String fileContent, String backupType) {
-        File file = new File(context.getCacheDir(), PathHelper.getBackupFilename(backupType));
-        if (FileHelper.saveToFile(file, fileContent)) {
-            FileHelper.shareFile(context, file);
-        } else {
-            Toast.makeText(context, R.string.backup_failed, Toast.LENGTH_LONG).show();
-        }
-    }
-
-    private void openBackup(PendingFileOperation pendingFileOperation) {
-        this.pendingFileOperation = pendingFileOperation;
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.setType("application/json");
-        openFileLauncher.launch(intent);
     }
 
     private enum PendingFileOperation {NONE, MEDICINE, REMINDER_EVENTS}
