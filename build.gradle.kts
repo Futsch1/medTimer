ac66e043b2d3ab97d3a589bded87c20ae9d5a699
Futsch1 <florian.fetz@googlemail.com>
2024-09-27 15:27:47 +0200

    Bumped AGP Bumped dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.6.0" apply false
+    id("com.android.application") version "8.6.1" apply false
     id("androidx.room") version "2.6.1" apply false
-    id("androidx.navigation.safeargs") version "2.8.0" apply false
+    id("androidx.navigation.safeargs") version "2.8.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
 
