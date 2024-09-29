ab10ab1e7f6ab3bc11c878c1abdd8bd16980c62b
Futsch1 <florian.fetz@googlemail.com>
2024-09-29 11:26:35 +0200

    Bumped version to v1.10.0-rc.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 72
-        versionName = "1.10.0"
+        versionName = "1.10.0-rc.1"
         base.archivesName = "MedTimer"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
