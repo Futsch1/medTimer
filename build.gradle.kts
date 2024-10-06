e53e2b754be9f6fb189600e26437900cb760223b
Futsch1 <florian.fetz@googlemail.com>
2024-10-06 12:52:24 +0200

    Bumped navigation.safeargs version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.7.0" apply false
     id("androidx.room") version "2.6.1" apply false
-    id("androidx.navigation.safeargs") version "2.8.1" apply false
+    id("androidx.navigation.safeargs") version "2.8.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
 
