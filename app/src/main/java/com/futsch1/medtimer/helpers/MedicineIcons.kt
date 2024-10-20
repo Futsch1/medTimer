e5989f7a39c115a4b6d140ca5739a7137b07a7f5
Futsch1 <florian.fetz@googlemail.com>
2024-10-20 13:17:05 +0200

    Re-enable notification icon ID

--- app/src/main/java/com/futsch1/medtimer/helpers/MedicineIcons.kt
+++ app/src/main/java/com/futsch1/medtimer/helpers/MedicineIcons.kt
@@ -3,9 +3,11 @@ package com.futsch1.medtimer.helpers
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.graphics.drawable.DrawableCompat
+import androidx.preference.PreferenceManager
 import com.futsch1.medtimer.R
 import com.google.android.material.color.MaterialColors
 import com.maltaisn.icondialog.pack.IconDrawableLoader
@@ -15,14 +17,24 @@ import com.maltaisn.icondialog.pack.IconPackLoader
 
 class MedicineIcons(context: Context) {
     private val defaultDrawable = AppCompatResources.getDrawable(context, R.drawable.capsule)!!
-    private val iconColor =
-        MaterialColors.getColor(context, com.google.android.material.R.attr.colorOnSurface, 0)
+    private var iconColor =
+        MaterialColors.getColor(
+            context,
+            com.google.android.material.R.attr.colorOnSurface,
+            0
+        )
 
     init {
         IconPackLoader(context).load(R.xml.icon_pack)
         if (pack == null) {
             pack = IconPackLoader(context).load(R.xml.icon_pack)
             pack!!.loadDrawables(IconDrawableLoader(context))
+        }
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+        if (iconColor != 0) {
+            sharedPref.edit().putInt("icon_color", iconColor).apply()
+        } else {
+            iconColor = sharedPref.getInt("icon_color", Color.BLACK)
         }
     }
 
