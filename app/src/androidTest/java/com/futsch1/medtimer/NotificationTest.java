0da961256e6fe0e9f83fc4339603d122ffc222cf
Futsch1 <florian.fetz@googlemail.com>
2025-01-03 13:11:42 +0100

    Select icon and color in notification test

--- app/src/androidTest/java/com/futsch1/medtimer/NotificationTest.java
+++ app/src/androidTest/java/com/futsch1/medtimer/NotificationTest.java
@@ -5,11 +5,20 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withResourceName;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.assertNotNull;
 
+import android.view.InputDevice;
+import android.view.MotionEvent;
+
+import androidx.test.espresso.action.GeneralClickAction;
+import androidx.test.espresso.action.GeneralLocation;
+import androidx.test.espresso.action.Press;
+import androidx.test.espresso.action.Tap;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.uiautomator.By;
@@ -31,6 +40,16 @@ public class NotificationTest extends BaseTestHelper {
     @Test
     public void notificationTest() {
         AndroidTestHelper.createMedicine("Test med");
+        
+        // Set color and icon
+        onView(withId(R.id.enableColor)).perform(click());
+        onView(withId(R.id.selectColor)).perform(click());
+        onView(withResourceName("colorPickerView")).perform(new GeneralClickAction(Tap.SINGLE, GeneralLocation.CENTER_LEFT, Press.FINGER, InputDevice.SOURCE_UNKNOWN, MotionEvent.BUTTON_PRIMARY));
+        onView(withId(android.R.id.button1)).perform(click());
+
+        onView(withId(R.id.selectIcon)).perform(click());
+        onView(withResourceName("icd_rcv_icon_list")).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+
         AndroidTestHelper.createReminder("1", AndroidTestHelper.getNextNotificationTime().toLocalTime());
 
         onView(withId(R.id.open_advanced_settings)).perform(click());
