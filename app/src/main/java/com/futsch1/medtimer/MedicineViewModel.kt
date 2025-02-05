028504de7de0c21d7dd5a6b2a8e3f2ee926011a7
Futsch1 <florian.fetz@googlemail.com>
2025-02-05 21:37:15 +0100

    Also filter next reminders

--- app/src/main/java/com/futsch1/medtimer/MedicineViewModel.kt
+++ app/src/main/java/com/futsch1/medtimer/MedicineViewModel.kt
@@ -18,14 +18,16 @@ class MedicineViewModel(application: Application) : AndroidViewModel(application
     private val liveMedicines: LiveData<List<MedicineWithReminders>> =
         medicineRepository.liveMedicines
 
-    private val validTagIds: MutableLiveData<Set<Int>> = MutableLiveData(setOf())
+    private val validTagIds: MutableLiveData<Set<Int>> = MutableLiveData()
     val tagFilterStore = TagFilterStore(application, validTagIds)
     private lateinit var medicineToTags: List<MedicineToTag>
 
     private val filteredMedicine = MediatorLiveData<List<MedicineWithReminders>>()
     val medicines: LiveData<List<MedicineWithReminders>> = filteredMedicine
 
-    val scheduledReminders = MutableLiveData<List<ScheduledReminder>>()
+    private val liveScheduledReminders: MutableLiveData<List<ScheduledReminder>> = MutableLiveData()
+    private val filteredScheduledReminders = MediatorLiveData<List<ScheduledReminder>>()
+    val scheduledReminders = filteredScheduledReminders
 
     init {
         medicineRepository.liveMedicineToTags.observeForever {
@@ -38,6 +40,35 @@ class MedicineViewModel(application: Application) : AndroidViewModel(application
         filteredMedicine.addSource(validTagIds) {
             filteredMedicine.value = getFilteredMedicine(liveMedicines, validTagIds)
         }
+
+        filteredScheduledReminders.addSource(liveScheduledReminders) {
+            filteredScheduledReminders.value =
+                getFilteredScheduledReminders(liveScheduledReminders, validTagIds)
+        }
+        filteredScheduledReminders.addSource(validTagIds) {
+            filteredScheduledReminders.value =
+                getFilteredScheduledReminders(liveScheduledReminders, validTagIds)
+        }
+    }
+
+    private fun getFilteredScheduledReminders(
+        liveData: MutableLiveData<List<ScheduledReminder>>,
+        validTagIds: MutableLiveData<Set<Int>>
+    ): List<ScheduledReminder> {
+        if (liveData.value.isNullOrEmpty()) {
+            return emptyList()
+        }
+        if (validTagIds.value.isNullOrEmpty()) {
+            return liveData.value!!
+        }
+        return liveData.value!!.stream()
+            .filter { medicine ->
+                filterMedicineId(
+                    medicine.medicine.medicineId,
+                    validTagIds.value
+                )
+            }
+            .collect(Collectors.toList())
     }
 
     private fun getFilteredMedicine(
@@ -85,6 +116,6 @@ class MedicineViewModel(application: Application) : AndroidViewModel(application
     }
 
     fun setScheduledReminders(scheduledReminders: List<ScheduledReminder>) {
-        this.scheduledReminders.value = scheduledReminders
+        this.liveScheduledReminders.value = scheduledReminders
     }
 }
