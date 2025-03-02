05e1bc5e149ebd755d7f58e1bf127147e6d99e73
Futsch1 <florian.fetz@googlemail.com>
2025-03-02 08:37:32 +0100

    Revert "Potential fix for code scanning alert no. 44: Insecure local authentication"
    This reverts commit 35af1d2b740cb217b0a598b56db7594f3ac1a440.

--- app/src/main/java/com/futsch1/medtimer/Biometrics.kt
+++ app/src/main/java/com/futsch1/medtimer/Biometrics.kt
@@ -10,13 +10,6 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import java.util.concurrent.Executor
 
-import android.security.keystore.KeyGenParameterSpec
-import android.security.keystore.KeyProperties
-import java.security.KeyStore
-import javax.crypto.Cipher
-import javax.crypto.KeyGenerator
-import javax.crypto.SecretKey
-import javax.crypto.spec.IvParameterSpec
 
 class Biometrics(val context: Context) {
     fun hasBiometrics(): Boolean {
@@ -29,40 +22,9 @@ class Biometrics(val context: Context) {
     private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
-    private fun generateSecretKey() {
-        val keyGenParameterSpec = KeyGenParameterSpec.Builder(
-            "MySecretKey",
-            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
-        )
-            .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
-            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
-            .setUserAuthenticationRequired(true)
-            .setInvalidatedByBiometricEnrollment(true)
-            .build()
-        val keyGenerator = KeyGenerator.getInstance(
-            KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore"
-        )
-        keyGenerator.init(keyGenParameterSpec)
-        keyGenerator.generateKey()
-    }
-
-    private fun getSecretKey(): SecretKey {
-        val keyStore = KeyStore.getInstance("AndroidKeyStore")
-        keyStore.load(null)
-        return keyStore.getKey("MySecretKey", null) as SecretKey
-    }
-
-    private fun getCipher(): Cipher {
-        return Cipher.getInstance(
-            "${KeyProperties.KEY_ALGORITHM_AES}/" +
-            "${KeyProperties.BLOCK_MODE_CBC}/" +
-            KeyProperties.ENCRYPTION_PADDING_PKCS7
-        )
-    }
-
     fun authenticate(
         activity: FragmentActivity,
-        successCallback: (ByteArray) -> Unit,
+        successCallback: () -> Unit,
         failureCallback: () -> Unit
     ) {
         executor = ContextCompat.getMainExecutor(context)
@@ -80,13 +42,7 @@ class Biometrics(val context: Context) {
                     result: BiometricPrompt.AuthenticationResult
                 ) {
                     super.onAuthenticationSucceeded(result)
-                    val cipher = result.cryptoObject?.cipher
-                    val decryptedData = cipher?.doFinal(encryptedData)
-                    if (decryptedData != null) {
-                        successCallback(decryptedData)
-                    } else {
-                        failureCallback()
-                    }
+                    successCallback()
                 }
 
                 override fun onAuthenticationFailed() {
@@ -100,14 +56,7 @@ class Biometrics(val context: Context) {
             .setAllowedAuthenticators(BIOMETRIC_WEAK or DEVICE_CREDENTIAL)
             .build()
 
-        generateSecretKey()
-        val cipher = getCipher()
-        val secretKey = getSecretKey()
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, IvParameterSpec(ByteArray(16)))
-
-        biometricPrompt.authenticate(
-            BiometricPrompt.CryptoObject(cipher),
-            promptInfo
-        )
+        @Suppress("kotlin:S6293") // No cryptography required inside app, only use authentication to protect app access
+        biometricPrompt.authenticate(promptInfo)
     }
 }
