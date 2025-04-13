636debda3b0d7d52f6d66baa84617ea7aa289fa2
Futsch1 <florian.fetz@googlemail.com>
2025-04-13 18:28:37 +0200

    Show out of stock icon in notification

--- app/src/main/java/com/futsch1/medtimer/database/Medicine.java
+++ app/src/main/java/com/futsch1/medtimer/database/Medicine.java
@@ -72,12 +72,12 @@ public class Medicine {
         this.unit = "";
     }
 
-    public boolean isStockManagementActive() {
-        return (amount != 0 || outOfStockReminder != OutOfStockReminderType.OFF || outOfStockReminderThreshold != 0);
+    public boolean isOutOfStock() {
+        return isStockManagementActive() && amount <= outOfStockReminderThreshold;
     }
 
-    public boolean isOutOfStock() {
-        return amount <= outOfStockReminderThreshold;
+    public boolean isStockManagementActive() {
+        return (amount != 0 || outOfStockReminder != OutOfStockReminderType.OFF || outOfStockReminderThreshold != 0);
     }
 
     public enum OutOfStockReminderType {
--- app/src/main/java/com/futsch1/medtimer/reminders/notificationFactory/BigReminderNotificationFactory.kt
+++ app/src/main/java/com/futsch1/medtimer/reminders/notificationFactory/BigReminderNotificationFactory.kt
@@ -1,7 +1,9 @@
 package com.futsch1.medtimer.reminders.notificationFactory
 
 import android.content.Context
+import android.text.SpannableStringBuilder
 import android.widget.RemoteViews
+import androidx.core.text.bold
 import com.futsch1.medtimer.R
 import com.futsch1.medtimer.database.FullMedicine
 import com.futsch1.medtimer.database.Reminder
@@ -29,8 +31,10 @@ class BigReminderNotificationFactory(
             MedicineHelper.getMedicineName(context, medicine.medicine, true)
         val stockString =
             addLineBreakIfNotEmpty(MedicineHelper.getStockText(context, medicine.medicine))
-        val notificationString =
-            "$medicineNameString (${reminder.amount})\n${getInstructions()}$stockString$remindTime"
+        val baseString = SpannableStringBuilder().bold { append(medicineNameString) }
+            .append(" (${reminder.amount})")
+        val notificationString = SpannableStringBuilder().append(baseString)
+            .append("\n${getInstructions()}$stockString$remindTime")
 
         views.setTextViewText(
             R.id.notificationTitle,
@@ -40,8 +44,15 @@ class BigReminderNotificationFactory(
         views.setOnClickPendingIntent(R.id.takenButton, getTakenPendingIntent())
         views.setOnClickPendingIntent(R.id.skippedButton, getSkippedPendingIntent())
         views.setOnClickPendingIntent(R.id.snoozeButton, getSnoozePendingIntent())
+        views.setTextViewCompoundDrawablesRelative(
+            R.id.notificationTitle,
+            if (medicine.medicine.isOutOfStock) R.drawable.box_seam else 0,
+            0,
+            0,
+            0
+        )
 
         builder.setCustomBigContentView(views)
-        builder.setContentText("$medicineNameString (${reminder.amount})")
+        builder.setContentText(baseString)
     }
 }
