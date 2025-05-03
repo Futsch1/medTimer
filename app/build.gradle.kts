a4b3fcaad31a709241cde4be53175ce7ef213ad5
Futsch1 <florian.fetz@googlemail.com>
2025-05-03 17:38:46 +0200

    Bump version to v1.17.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         minSdk = 28
         multiDexEnabled = true
         targetSdk = 36
-        versionCode = 116
-        versionName = "1.17.0-rc.2"
+        versionCode = 117
+        versionName = "1.17.0"
         base.archivesName = "MedTimer"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
