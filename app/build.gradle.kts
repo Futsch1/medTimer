f8f964f2e104524900e1631c03fbe65a2916f081
Futsch1 <florian.fetz@googlemail.com>
2025-06-07 10:43:39 +0200

    Bump versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,7 +82,7 @@ android {
 }
 
 dependencies {
-    val appcompatVersion = "1.7.0"
+    val appcompatVersion = "1.7.1"
     val materialVersion = "1.12.0"
     val constraintLayoutVersion = "2.2.1"
     val androidXNavigationVersion = "2.9.0"
@@ -90,7 +90,7 @@ dependencies {
     val lifecycleExtensionsVersion = "2.2.0"
     val workRuntimeVersion = "2.10.1"
     val coreKtxVersion = "1.16.0"
-    val lifecycleViewmodelKtxVersion = "2.9.0"
+    val lifecycleViewmodelKtxVersion = "2.9.1"
     val roomVersion = "2.7.1"
     val colorPickerViewVersion = "3.1.0"
     val simplyPDFVersion = "2.1.1"
