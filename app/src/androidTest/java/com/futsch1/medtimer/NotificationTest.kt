7d455ea2879696acef7be8df892cb7eb886493f6
Futsch1 <florian.fetz@googlemail.com>
2025-06-09 13:57:05 +0200

    Fix notification tests

--- app/src/androidTest/java/com/futsch1/medtimer/NotificationTest.kt
+++ app/src/androidTest/java/com/futsch1/medtimer/NotificationTest.kt
@@ -25,7 +25,6 @@ import com.adevinta.android.barista.interaction.BaristaDialogInteractions.clickD
 import com.adevinta.android.barista.interaction.BaristaEditTextInteractions.writeTo
 import com.adevinta.android.barista.interaction.BaristaMenuClickInteractions.openMenu
 import com.adevinta.android.barista.interaction.BaristaSleepInteractions.sleep
-import com.adevinta.android.barista.rule.flaky.AllowFlaky
 import com.futsch1.medtimer.AndroidTestHelper.MainMenu
 import com.futsch1.medtimer.AndroidTestHelper.navigateTo
 import org.hamcrest.Matchers.allOf
@@ -80,7 +79,7 @@ class NotificationTest : BaseTestHelper() {
     }
 
     @Test
-    @AllowFlaky(attempts = 1)
+    //@AllowFlaky(attempts = 1)
     fun actionOnDismissedNotification() {
         val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
         openMenu()
