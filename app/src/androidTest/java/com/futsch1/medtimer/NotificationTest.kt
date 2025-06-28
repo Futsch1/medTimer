d7afdc7b2c26d1e72dbad6aba6731587e511a7a7
Futsch1 <florian.fetz@googlemail.com>
2025-06-28 15:19:14 +0200

    Fix test on all versions

--- app/src/androidTest/java/com/futsch1/medtimer/NotificationTest.kt
+++ app/src/androidTest/java/com/futsch1/medtimer/NotificationTest.kt
@@ -25,6 +25,7 @@ import com.adevinta.android.barista.interaction.BaristaDialogInteractions.clickD
 import com.adevinta.android.barista.interaction.BaristaEditTextInteractions.writeTo
 import com.adevinta.android.barista.interaction.BaristaMenuClickInteractions.openMenu
 import com.adevinta.android.barista.interaction.BaristaSleepInteractions.sleep
+import com.adevinta.android.barista.rule.flaky.AllowFlaky
 import com.futsch1.medtimer.AndroidTestHelper.MainMenu
 import com.futsch1.medtimer.AndroidTestHelper.navigateTo
 import com.futsch1.medtimer.helpers.TimeHelper
@@ -320,7 +321,7 @@ class NotificationTest : BaseTestHelper() {
     }
 
     @Test
-    //@AllowFlaky(attempts = 1)
+    @AllowFlaky(attempts = 1)
     fun sameTimeReminders() {
         val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
@@ -342,11 +343,17 @@ class NotificationTest : BaseTestHelper() {
         sleep(2_000)
         val notification = device.wait(Until.findObject(By.textContains(TEST_MED)), 240_000)
         assertNotNull(notification)
-        device.pressBack()
 
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            device.pressBack()
+        }
         // We need to wait until all notifications have appeared
         sleep(2_000)
-        device.openNotification()
+
+        // Only do this if SDK > 28
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            device.openNotification()
+        }
 
         val button = device.findObject(By.text(getNotificationText(R.string.all_taken, notificationTimeString)))
         internalAssert(button != null)
