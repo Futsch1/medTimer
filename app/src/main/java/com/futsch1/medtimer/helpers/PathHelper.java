1cdd6d5cc4dcf0eb7c6be3d785a44d86a42807de
Futsch1 <florian.fetz@googlemail.com>
2025-07-10 21:59:34 +0200

    Make export file name stable in file sorting use case

--- app/src/main/java/com/futsch1/medtimer/helpers/PathHelper.java
+++ app/src/main/java/com/futsch1/medtimer/helpers/PathHelper.java
@@ -4,7 +4,6 @@ import com.futsch1.medtimer.exporters.Exporter;
 
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 
 public class PathHelper {
     private static final String RESERVED_CHARS = "[\\[|?*<\":>+/'\\],]";
@@ -14,12 +13,12 @@ public class PathHelper {
     }
 
     public static String getExportFilename(Exporter exporter) {
-        String fileName = String.format("MedTimer Export %s.%s", ZonedDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)), exporter.getExtension());
+        String fileName = String.format("MedTimer_Export_%s.%s", ZonedDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")), exporter.getExtension());
         return fileName.replaceAll(RESERVED_CHARS, "_");
     }
 
     public static String getBackupFilename() {
-        String fileName = String.format("MedTimer Backup %s.json", ZonedDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)));
+        String fileName = String.format("MedTimer_Backup_%s.json", ZonedDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")));
         return fileName.replaceAll(RESERVED_CHARS, "_");
     }
 }
