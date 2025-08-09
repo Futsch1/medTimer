5f5f1caf6ad17d23e448bc5696d0e0404ea8ccea
Futsch1 <florian.fetz@googlemail.com>
2025-08-09 23:40:47 +0200

    Initial medicine PDF export

--- app/src/main/java/com/futsch1/medtimer/helpers/TableHelper.java
+++ app/src/main/java/com/futsch1/medtimer/helpers/TableHelper.java
@@ -12,7 +12,7 @@ public class TableHelper {
         // Intended empty
     }
 
-    public static List<String> getTableHeadersForExport(Context context) {
+    public static List<String> getTableHeadersForEventExport(Context context) {
         final String[] headerTexts = {
                 context.getString(R.string.reminded),
                 context.getString(R.string.name),
@@ -23,6 +23,15 @@ public class TableHelper {
                 context.getString(R.string.notes),
                 context.getString(R.string.reminded) + " (ISO 8601)",
                 context.getString(R.string.taken) + " (ISO 8601)"};
+        return Arrays.asList(headerTexts);
+    }
+
+    public static List<String> getTableHeadersForMedicationExport(Context context) {
+        final String[] headerTexts = {
+                context.getString(R.string.tab_medicine),
+                context.getString(R.string.dosage),
+                context.getString(R.string.time)
+        };
         return Arrays.asList(headerTexts);
     }
 
