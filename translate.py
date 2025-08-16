5c178d554ab05b70e5c843f579465c9f3c654412
Futsch1 <florian.fetz@googlemail.com>
2025-08-16 11:07:07 +0200

    Syntax fix

--- translate.py
+++ translate.py
@@ -99,7 +99,7 @@ for string_name, language in translate_list:
     new_elements[language].text = escape(translated_string.text)
 
 # Collect all languages where a translation string was found
-languages = {[tl[1] for tl in translate_list])}
+languages = {[tl[1] for tl in translate_list]}
 
 # Update the existing strings.xml files
 for language in languages:
