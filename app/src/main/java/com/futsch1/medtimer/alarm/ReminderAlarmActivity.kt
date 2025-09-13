9c15df0f62e32aa531d561b7412d516db179a480
Futsch1 <florian.fetz@googlemail.com>
2025-09-13 23:40:02 +0200

    Vibrate and play sound in alarm

--- app/src/main/java/com/futsch1/medtimer/alarm/ReminderAlarmActivity.kt
+++ app/src/main/java/com/futsch1/medtimer/alarm/ReminderAlarmActivity.kt
@@ -3,7 +3,14 @@ package com.futsch1.medtimer.alarm
 import android.app.KeyguardManager
 import android.content.Context
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
+import android.provider.Settings
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
@@ -15,6 +22,9 @@ import com.futsch1.medtimer.R
 import com.futsch1.medtimer.database.ReminderEvent
 
 class ReminderAlarmActivity() : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var vibrator: Vibrator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,7 +39,40 @@ class ReminderAlarmActivity() : AppCompatActivity() {
             WindowCompat.getInsetsController(window, window.decorView)
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
 
+        val bundle = Bundle()
+        bundle.putInt(EXTRA_REMINDER_EVENT_ID, 1)
+        bundle.putInt(EXTRA_NOTIFICATION_ID, 2)
+        bundle.putString(EXTRA_NOTIFICATION_TIME_STRING, "12")
+        intent.putExtras(bundle)
+
         addAlarmFragment(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startAlarmTone()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer.stop()
+        mediaPlayer.release()
+        vibrator.cancel()
+    }
+
+    private fun startAlarmTone() {
+        val alarmURI = Settings.System.DEFAULT_ALARM_ALERT_URI
+        mediaPlayer = MediaPlayer.create(this, alarmURI, null, AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_ALARM).build(), 0)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
+
+        vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            (getSystemService(VIBRATOR_MANAGER_SERVICE) as VibratorManager).defaultVibrator
+        } else {
+            @Suppress("DEPRECATION")
+            getSystemService(VIBRATOR_SERVICE) as Vibrator
+        }
+        vibrator.vibrate(VibrationEffect.createWaveform(longArrayOf(500, 500), 0))
     }
 
     override fun onNewIntent(intent: Intent?) {
