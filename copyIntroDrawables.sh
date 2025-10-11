e5756d049f923e0efb92302f4491cc66130a3d92
Futsch1 <florian.fetz@googlemail.com>
2025-10-11 10:59:00 +0200

    Fix fastlane setup

--- copyIntroDrawables.sh
+++ copyIntroDrawables.sh
@@ -10,13 +10,15 @@ done
 cp fastlane/metadata/android/ar-AE/ fastlane/metadata/android/ar -R
 rm -r fastlane/metadata/android/ar-AE
 
-locales=('ar' 'bg' 'da-DK' 'de-DE' 'es-ES' 'el-GR' 'fr-FR' 'it-IT' 'nl-NL' 'pl-PL' 'pt-BR' 'ru-RU' 'sv-SE' 'ta-IN' 'tr-TR' 'uk' 'zh-rCN' 'zh-rTW')
+locales=('ar' 'bg' 'da-DK' 'de-DE' 'es-ES' 'el-GR' 'fr-FR' 'it-IT' 'nl-NL' 'pl-PL' 'pt-BR' 'ru-RU' 'sv-SE' 'ta-IN' 'tr-TR' 'uk' 'zh-CN' 'zh-TW')
 
 for l in "${locales[@]}"; do
 	mkdir ./app/src/main/res/drawable-"${l:0:2}" 2>/dev/null
 	for i in $(seq 0 4); do
 		if [ -d ./app/src/main/res/drawable-"${l}" ]; then
 			cp -f -r ./fastlane/metadata/android/"${l}"/images/phoneScreenshots/"${screenshots[$i]}".png ./app/src/main/res/drawable-"${l}"/intro_"${names[$i]}".png
+		elif [ -d ./app/src/main/res/drawable-"${l:0:2}"r"${l:2:}" ] ; then
+			cp -f -r ./fastlane/metadata/android/"${l}"/images/phoneScreenshots/"${screenshots[$i]}".png ./app/src/main/res/drawable-"${l:0:2}"r"${l:2:}"/intro_"${names[$i]}".png
 		else
 			cp -f -r ./fastlane/metadata/android/"${l}"/images/phoneScreenshots/"${screenshots[$i]}".png ./app/src/main/res/drawable-"${l:0:2}"/intro_"${names[$i]}".png
 		fi
