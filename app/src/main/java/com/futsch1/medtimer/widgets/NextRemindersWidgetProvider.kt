3e9a10a9b8a9269acd5e8cc812f97e3d6e5029e8
Futsch1 <florian.fetz@googlemail.com>
2025-12-21 22:15:39 +0100

    Fix qlty maintainability issue

--- app/src/main/java/com/futsch1/medtimer/widgets/NextRemindersWidgetProvider.kt
+++ app/src/main/java/com/futsch1/medtimer/widgets/NextRemindersWidgetProvider.kt
@@ -1,30 +1,9 @@
 package com.futsch1.medtimer.widgets
 
-import android.appwidget.AppWidgetManager
-import android.appwidget.AppWidgetProvider
 import android.content.Context
 import com.futsch1.medtimer.R
 
-class NextRemindersWidgetProvider : AppWidgetProvider() {
-
-    override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray
-    ) {
-        performWidgetUpdate(
-            getNextReminderWidgetImpl(context), appWidgetIds, appWidgetManager
-        )
-    }
-
-    override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
-    }
-
-    override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
-    }
-}
+class NextRemindersWidgetProvider : WidgetProvider(::getNextReminderWidgetImpl)
 
 fun getNextReminderWidgetImpl(context: Context): WidgetImpl {
     val lineProvider = NextRemindersLineProvider(context)
