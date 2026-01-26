72bfe66345876922154dbdcad8e2781202df62da
Futsch1 <63090558+Futsch1@users.noreply.github.com>
2026-01-26 17:31:52 +0100

    Show reminder type icon in overview

--- app/src/main/java/com/futsch1/medtimer/database/Reminder.kt
+++ app/src/main/java/com/futsch1/medtimer/database/Reminder.kt
@@ -1,9 +1,11 @@
 package com.futsch1.medtimer.database
 
+import androidx.annotation.DrawableRes
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Ignore
 import androidx.room.PrimaryKey
+import com.futsch1.medtimer.R
 import com.google.gson.annotations.Expose
 import java.util.Objects
 
@@ -137,18 +139,30 @@ class Reminder(@JvmField var medicineRelId: Int) {
 
     val reminderType: ReminderType
         get() {
-            return if (linkedReminderId != 0) {
-                ReminderType.LINKED
-            } else if (intervalStart != 0L && !windowedInterval) {
-                ReminderType.CONTINUOUS_INTERVAL
-            } else if (windowedInterval) {
-                ReminderType.WINDOWED_INTERVAL
-            } else if (outOfStockReminderType != OutOfStockReminderType.OFF) {
-                ReminderType.OUT_OF_STOCK
-            } else if (expirationReminderType != ExpirationReminderType.OFF) {
-                ReminderType.EXPIRATION_DATE
-            } else {
-                ReminderType.TIME_BASED
+            return when {
+                linkedReminderId != 0 -> {
+                    ReminderType.LINKED
+                }
+
+                intervalStart != 0L && !windowedInterval -> {
+                    ReminderType.CONTINUOUS_INTERVAL
+                }
+
+                windowedInterval -> {
+                    ReminderType.WINDOWED_INTERVAL
+                }
+
+                outOfStockReminderType != OutOfStockReminderType.OFF -> {
+                    ReminderType.OUT_OF_STOCK
+                }
+
+                expirationReminderType != ExpirationReminderType.OFF -> {
+                    ReminderType.EXPIRATION_DATE
+                }
+
+                else -> {
+                    ReminderType.TIME_BASED
+                }
             }
         }
 
@@ -195,14 +209,15 @@ class Reminder(@JvmField var medicineRelId: Int) {
                 outOfStockThreshold == that.outOfStockThreshold && outOfStockReminderType == that.outOfStockReminderType && expirationReminderType == that.expirationReminderType
     }
 
-    enum class ReminderType {
-        TIME_BASED,
-        CONTINUOUS_INTERVAL,
-        LINKED,
-        WINDOWED_INTERVAL,
-        OUT_OF_STOCK,
-        EXPIRATION_DATE
+    enum class ReminderType(@param:DrawableRes val icon: Int) {
+        TIME_BASED(R.drawable.calendar_event),
+        CONTINUOUS_INTERVAL(R.drawable.repeat),
+        LINKED(R.drawable.link),
+        WINDOWED_INTERVAL(R.drawable.interval),
+        OUT_OF_STOCK(R.drawable.box_seam),
+        EXPIRATION_DATE(R.drawable.ban)
     }
+
 
     enum class OutOfStockReminderType {
         ONCE,
