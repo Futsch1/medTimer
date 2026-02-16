8d4a29543d05e12f77bedd1e5e3081a164b9f172
Futsch1 <63090558+Futsch1@users.noreply.github.com>
2026-02-16 21:51:53 +0100

    Use consts for medicine names

--- app/src/androidTest/java/com/futsch1/medtimer/MedicineHandlingTest.kt
+++ app/src/androidTest/java/com/futsch1/medtimer/MedicineHandlingTest.kt
@@ -9,33 +9,39 @@ import com.adevinta.android.barista.interaction.BaristaMenuClickInteractions.ope
 import com.futsch1.medtimer.RecyclerViewDragAction.drag
 import org.junit.Test
 
+const val TEST_MED_1 = "Test"
+const val TEST_MED_2 = "Test2"
+const val TEST_MED_3 = "A test"
+
 class MedicineHandlingTest : BaseTestHelper() {
+
+
     @Test
     //@AllowFlaky(attempts = 1)
     fun medicineMoveTest() {
-        AndroidTestHelper.createMedicine("Test")
-        AndroidTestHelper.createMedicine("Test2")
+        AndroidTestHelper.createMedicine(TEST_MED_1)
+        AndroidTestHelper.createMedicine(TEST_MED_2)
 
         pressBack()
 
-        assertDisplayedAtPosition(R.id.medicineList, 0, R.id.medicineName, "Test")
+        assertDisplayedAtPosition(R.id.medicineList, 0, R.id.medicineName, TEST_MED_1)
 
         onView(withId(R.id.medicineList)).perform(drag(0, 1))
-        assertDisplayedAtPosition(R.id.medicineList, 0, R.id.medicineName, "Test2")
+        assertDisplayedAtPosition(R.id.medicineList, 0, R.id.medicineName, TEST_MED_2)
 
         onView(withId(R.id.medicineList)).perform(drag(1, 0))
-        assertDisplayedAtPosition(R.id.medicineList, 0, R.id.medicineName, "Test")
+        assertDisplayedAtPosition(R.id.medicineList, 0, R.id.medicineName, TEST_MED_1)
 
-        AndroidTestHelper.createMedicine("A test3")
+        AndroidTestHelper.createMedicine(TEST_MED_3)
         pressBack()
 
-        assertDisplayedAtPosition(R.id.medicineList, 2, R.id.medicineName, "A test3")
+        assertDisplayedAtPosition(R.id.medicineList, 2, R.id.medicineName, TEST_MED_3)
 
         openMenu()
         clickOn(R.string.sort)
         clickOn(R.string.by_name)
-        assertDisplayedAtPosition(R.id.medicineList, 0, R.id.medicineName, "A test3")
-        assertDisplayedAtPosition(R.id.medicineList, 1, R.id.medicineName, "Test")
-        assertDisplayedAtPosition(R.id.medicineList, 2, R.id.medicineName, "Test2")
+        assertDisplayedAtPosition(R.id.medicineList, 0, R.id.medicineName, TEST_MED_3)
+        assertDisplayedAtPosition(R.id.medicineList, 1, R.id.medicineName, TEST_MED_1)
+        assertDisplayedAtPosition(R.id.medicineList, 2, R.id.medicineName, TEST_MED_2)
     }
 }
