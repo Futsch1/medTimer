4afb082bf533e15cd5bbb73767f3a50ccda52f31
Futsch1 <63090558+Futsch1@users.noreply.github.com>
2026-02-21 10:17:20 +0100

    Bump version to v1.22.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         minSdk = 28
         multiDexEnabled = true
         targetSdk = 36
-        versionCode = 157
-        versionName = "1.22.5"
+        versionCode = 158
+        versionName = "1.22.6"
         base.archivesName = "MedTimer"
         // Use this deprecated setting because Android Lint will not pick up androidResources.localeFilters correctly
         @Suppress("DEPRECATION")
