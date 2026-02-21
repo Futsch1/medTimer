16e4f66fd3c03aa4c4cfac0e4dd72c468571d854
Futsch1 <63090558+Futsch1@users.noreply.github.com>
2026-02-21 09:38:24 +0100

    Fix update of widget in SDK >= 31

--- app/src/main/java/com/futsch1/medtimer/widgets/WidgetImpl.kt
+++ app/src/main/java/com/futsch1/medtimer/widgets/WidgetImpl.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.util.SizeF
 import android.view.View
 import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
 import com.futsch1.medtimer.MainActivity
 import com.futsch1.medtimer.R
 
@@ -35,10 +36,10 @@ class WidgetImpl(
 
         val remoteViews = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val viewMapping: Map<SizeF, RemoteViews> = mapOf(
-                SizeF(110f, 50f) to createRemoteViews(1, true),
-                SizeF(200f, 50f) to createRemoteViews(1, false),
-                SizeF(110f, 150f) to createRemoteViews(4, true),
-                SizeF(200f, 150f) to createRemoteViews(4, false)
+                SizeF(110f, 50f) to buildRemoteViews(1, true),
+                SizeF(200f, 50f) to buildRemoteViews(1, false),
+                SizeF(110f, 150f) to buildRemoteViews(4, true),
+                SizeF(200f, 150f) to buildRemoteViews(4, false)
             )
             RemoteViews(viewMapping)
         } else {
@@ -47,6 +48,26 @@ class WidgetImpl(
             createRemoteViews(numLines, isSmall)
         }
         appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun buildRemoteViews(numLines: Int, isShort: Boolean): RemoteViews {
+        val layoutId = if (numLines == 1) widgetIds.smallWidgetLayoutId else widgetIds.widgetLayoutId
+        val remoteViews = RemoteViews(context.packageName, layoutId)
+
+        val builder = RemoteViews.RemoteCollectionItems.Builder()
+        for (i in 0..<numLines) {
+            val rv = RemoteViews(context.packageName, R.layout.widget_line)
+            rv.setTextViewText(R.id.widgetLineText, lineProvider.getWidgetLine(i, isShort))
+            builder.addItem(i.toLong(), rv)
+        }
+        remoteViews.setRemoteAdapter(R.id.widgetLines, builder.build())
+        remoteViews.setOnClickPendingIntent(
+            widgetIds.widgetId,
+            getOpenAppPendingIntent()
+        )
+
+        return remoteViews
     }
 
     private fun createRemoteViews(numLines: Int, isShort: Boolean): RemoteViews {
--- app/src/main/java/com/futsch1/medtimer/widgets/WidgetUpdateReceiver.kt
+++ app/src/main/java/com/futsch1/medtimer/widgets/WidgetUpdateReceiver.kt
@@ -5,29 +5,39 @@ import android.content.BroadcastReceiver
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class WidgetUpdateReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        val appWidgetManager = AppWidgetManager.getInstance(context)
+        val pendingResult = goAsync()
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val appWidgetManager = AppWidgetManager.getInstance(context)
 
-        val appWidgetIdsNextReminders = appWidgetManager.getAppWidgetIds(
-            ComponentName(
-                context!!,
-                NextRemindersWidgetProvider::class.java
-            )
-        )
-        performWidgetUpdate(
-            getNextReminderWidgetImpl(context), appWidgetIdsNextReminders, appWidgetManager
-        )
+                val appWidgetIdsNextReminders = appWidgetManager.getAppWidgetIds(
+                    ComponentName(
+                        context!!,
+                        NextRemindersWidgetProvider::class.java
+                    )
+                )
+                performWidgetUpdate(
+                    getNextReminderWidgetImpl(context), appWidgetIdsNextReminders, appWidgetManager
+                )
 
-        val appWidgetIdsLatestReminders = appWidgetManager.getAppWidgetIds(
-            ComponentName(
-                context,
-                LatestRemindersWidgetProvider::class.java
-            )
-        )
-        performWidgetUpdate(
-            getLatestReminderWidgetImpl(context), appWidgetIdsLatestReminders, appWidgetManager
-        )
+                val appWidgetIdsLatestReminders = appWidgetManager.getAppWidgetIds(
+                    ComponentName(
+                        context,
+                        LatestRemindersWidgetProvider::class.java
+                    )
+                )
+                performWidgetUpdate(
+                    getLatestReminderWidgetImpl(context), appWidgetIdsLatestReminders, appWidgetManager
+                )
+            } finally {
+                pendingResult.finish()
+            }
+        }
     }
 }
