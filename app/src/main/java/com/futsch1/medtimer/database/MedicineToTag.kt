182a6e730be9e48c53e14cdd682a1d0f016ad687
Futsch1 <63090558+Futsch1@users.noreply.github.com>
2026-03-06 09:55:33 +0100

    Convert more database entities from Java to Kotlin

--- app/src/main/java/com/futsch1/medtimer/database/MedicineToTag.kt
+++ app/src/main/java/com/futsch1/medtimer/database/MedicineToTag.kt
@@ -1,17 +1,8 @@
-package com.futsch1.medtimer.database;
+package com.futsch1.medtimer.database
 
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
+import androidx.room.ColumnInfo
+import androidx.room.Entity
 
-@Entity(primaryKeys = {"medicineId", "tagId"})
-public class MedicineToTag {
-    public int medicineId;
-    @ColumnInfo(index = true)
-    public int tagId;
-
-    public MedicineToTag(int medicineId, int tagId) {
-        this.medicineId = medicineId;
-        this.tagId = tagId;
-    }
-}
+@Entity(primaryKeys = ["medicineId", "tagId"])
+class MedicineToTag(@JvmField var medicineId: Int, @JvmField @field:ColumnInfo(index = true) var tagId: Int)
 
