cd04577856b81a8e7a2d6e8011580ef6190fa4ee
thomas kiljanczyk dev <thomas.kiljanczyk.dev@gmail.com>
2026-03-08 20:43:03 +0100

    #1260 migrate JSONReminderEventBackupUnitTest to Kotlin

--- app/src/test/java/com/futsch1/medtimer/JSONReminderEventBackupUnitTest.kt
+++ app/src/test/java/com/futsch1/medtimer/JSONReminderEventBackupUnitTest.kt
@@ -1,47 +1,36 @@
-package com.futsch1.medtimer;
+package com.futsch1.medtimer
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import android.graphics.Color
+import com.futsch1.medtimer.database.JSONReminderEventBackup
+import com.futsch1.medtimer.database.Reminder
+import com.futsch1.medtimer.database.ReminderEvent
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
-import android.graphics.Color;
-
-import com.futsch1.medtimer.database.JSONReminderEventBackup;
-import com.futsch1.medtimer.database.Reminder;
-import com.futsch1.medtimer.database.ReminderEvent;
-
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-class JSONReminderEventBackupUnitTest {
-
+internal class JSONReminderEventBackupUnitTest {
     // creates a backup object with a version number and a medicines array
     @Test
-    void testBackup() {
-        JSONReminderEventBackup jsonReminderEventBackup = new JSONReminderEventBackup();
-        List<ReminderEvent> reminderEvents = new ArrayList<>();
-        ReminderEvent reminderEvent = new ReminderEvent();
-        reminderEvent.setMedicineName("Medicine A");
-        reminderEvent.setColor(Color.RED);
-        reminderEvent.setUseColor(true);
-        reminderEvent.setAmount("1");
-        reminderEvent.setStatus(ReminderEvent.ReminderStatus.TAKEN);
-        reminderEvent.setRemindedTimestamp(1);
-        reminderEvent.setProcessedTimestamp(2);
-        reminderEvent.setReminderId(3);
-        reminderEvent.setIconId(4);
-        List<String> tags = new ArrayList<>();
-        tags.add("Tag A");
-        reminderEvent.setTags(tags);
-        reminderEvent.setLastIntervalReminderTimeInMinutes(12);
-        reminderEvent.setNotes("Notes");
-        reminderEvent.setReminderType(Reminder.ReminderType.LINKED);
-        reminderEvents.add(reminderEvent);
+    fun testBackup() {
+        val reminderEvents = listOf(ReminderEvent().apply {
+            medicineName = "Medicine A"
+            color = Color.RED
+            useColor = true
+            amount = "1"
+            status = ReminderEvent.ReminderStatus.TAKEN
+            remindedTimestamp = 1
+            processedTimestamp = 2
+            reminderId = 3
+            iconId = 4
 
-        String result = jsonReminderEventBackup.createBackupAsString(1, reminderEvents);
+            tags = listOf("Tag A")
+            lastIntervalReminderTimeInMinutes = 12
+            notes = "Notes"
+            reminderType = Reminder.ReminderType.LINKED
+        })
 
-        assertNotNull(result);
+        val jsonReminderEventBackup = JSONReminderEventBackup()
+        val result = assertNotNull(jsonReminderEventBackup.createBackupAsString(1, reminderEvents))
 
         // @formatter:off
         assertEquals("""
@@ -66,35 +55,44 @@ class JSONReminderEventBackupUnitTest {
       "reminderType": "LINKED"
     }
   ]
-}""", result);
-        // @formatter:on
-
-        List<ReminderEvent> parsedReminders = jsonReminderEventBackup.parseBackup(result);
-        assertNotNull(parsedReminders);
-        compareListReminderEvents(parsedReminders, reminderEvents);
+}
+""".trimIndent(), result)
+        
+        val parsedReminders =
+            assertNotNull(jsonReminderEventBackup.parseBackup(result))
+        compareListReminderEvents(parsedReminders, reminderEvents)
     }
 
-    private void compareListReminderEvents(List<ReminderEvent> actual, List<ReminderEvent> expected) {
-        assertEquals(expected.size(), actual.size());
-        for (int i = 0; i < actual.size(); i++) {
-            compareReminderEvent(actual.get(i), expected.get(i));
+    private fun compareListReminderEvents(
+        actual: List<ReminderEvent>,
+        expected: List<ReminderEvent>
+    ) {
+        assertEquals(expected.size, actual.size)
+        for (i in actual.indices) {
+            compareReminderEvent(actual.get(i), expected.get(i))
         }
     }
 
-    private void compareReminderEvent(ReminderEvent reminderEvent1, ReminderEvent reminderEvent2) {
-        assertEquals(reminderEvent1.getMedicineName(), reminderEvent2.getMedicineName());
-        assertEquals(reminderEvent1.getColor(), reminderEvent2.getColor());
-        assertEquals(reminderEvent1.getUseColor(), reminderEvent2.getUseColor());
-        assertEquals(reminderEvent1.getAmount(), reminderEvent2.getAmount());
-        assertEquals(reminderEvent1.getStatus(), reminderEvent2.getStatus());
-        assertEquals(reminderEvent1.getRemindedTimestamp(), reminderEvent2.getRemindedTimestamp());
-        assertEquals(reminderEvent1.getProcessedTimestamp(), reminderEvent2.getProcessedTimestamp());
-        assertEquals(reminderEvent1.getReminderId(), reminderEvent2.getReminderId());
-        assertEquals(reminderEvent1.getIconId(), reminderEvent2.getIconId());
-        assertEquals(reminderEvent1.getTags(), reminderEvent2.getTags());
-        assertEquals(reminderEvent1.getLastIntervalReminderTimeInMinutes(), reminderEvent2.getLastIntervalReminderTimeInMinutes());
-        assertEquals(reminderEvent1.getNotes(), reminderEvent2.getNotes());
-        assertEquals(reminderEvent1.getReminderType(), reminderEvent2.getReminderType());
+    private fun compareReminderEvent(reminderEvent1: ReminderEvent, reminderEvent2: ReminderEvent) {
+        assertEquals(reminderEvent1.medicineName, reminderEvent2.medicineName)
+        assertEquals(reminderEvent1.color, reminderEvent2.color)
+        assertEquals(reminderEvent1.useColor, reminderEvent2.useColor)
+        assertEquals(reminderEvent1.amount, reminderEvent2.amount)
+        assertEquals(reminderEvent1.status, reminderEvent2.status)
+        assertEquals(reminderEvent1.remindedTimestamp, reminderEvent2.remindedTimestamp)
+        assertEquals(
+            reminderEvent1.processedTimestamp,
+            reminderEvent2.processedTimestamp
+        )
+        assertEquals(reminderEvent1.reminderId, reminderEvent2.reminderId)
+        assertEquals(reminderEvent1.iconId, reminderEvent2.iconId)
+        assertEquals(reminderEvent1.tags, reminderEvent2.tags)
+        assertEquals(
+            reminderEvent1.lastIntervalReminderTimeInMinutes,
+            reminderEvent2.lastIntervalReminderTimeInMinutes
+        )
+        assertEquals(reminderEvent1.notes, reminderEvent2.notes)
+        assertEquals(reminderEvent1.reminderType, reminderEvent2.reminderType)
     }
 }
 
