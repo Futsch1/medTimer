bb53762ed035507ecc2a7fa264f57e270bbc7f7e
Futsch1 <63090558+Futsch1@users.noreply.github.com>
2026-03-13 17:00:34 +0100

    Fix unit test

--- app/src/test/java/com/futsch1/medtimer/schedulertests/ReminderSchedulerWeekendModeTest.kt
+++ app/src/test/java/com/futsch1/medtimer/schedulertests/ReminderSchedulerWeekendModeTest.kt
@@ -1,70 +1,28 @@
 package com.futsch1.medtimer.schedulertests
 
-import android.content.SharedPreferences
 import com.futsch1.medtimer.database.FullMedicine
-import com.futsch1.medtimer.di.DatastoreModule
-import com.futsch1.medtimer.di.DefaultPrefs
-import com.futsch1.medtimer.preferences.MedTimerPreferencesDataSource
-import com.futsch1.medtimer.preferences.PreferencesNames
+import com.futsch1.medtimer.preferences.MedTimerSettings
 import com.futsch1.medtimer.schedulertests.ReminderSchedulerUnitTest.Companion.scheduler
 import com.futsch1.medtimer.schedulertests.TestHelper.assertReminded
 import com.futsch1.medtimer.schedulertests.TestHelper.buildFullMedicine
 import com.futsch1.medtimer.schedulertests.TestHelper.buildReminder
 import com.futsch1.medtimer.schedulertests.TestHelper.on
-import dagger.hilt.android.testing.BindValue
-import dagger.hilt.android.testing.HiltAndroidRule
-import dagger.hilt.android.testing.HiltAndroidTest
-import dagger.hilt.android.testing.UninstallModules
-import org.junit.Before
-import org.junit.Rule
+import kotlinx.coroutines.flow.MutableStateFlow
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers
-import org.mockito.Mock
 import org.mockito.Mockito
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
 import java.time.DayOfWeek
 import java.time.LocalDate
-import javax.inject.Inject
 
-@UninstallModules(DatastoreModule::class)
-@HiltAndroidTest
-@RunWith(RobolectricTestRunner::class)
-@Config(application = dagger.hilt.android.testing.HiltTestApplication::class)
 internal class ReminderSchedulerWeekendModeTest {
-    @get:Rule
-    var hiltRule = HiltAndroidRule(this)
-
-    @Before
-    fun init() {
-        hiltRule.inject()
-    }
-
-    @BindValue
-    @DefaultPrefs
-    @Mock
-    val sharedPreferences: SharedPreferences = Mockito.mock(SharedPreferences::class.java)
-
-    @Inject
-    lateinit var preferencesDataSource: MedTimerPreferencesDataSource
-
     @Test
     fun weekendDaysEmpty() {
-        ReminderSchedulerUnitTest.preferencesDataSource = preferencesDataSource
         val scheduler = scheduler
 
+        val stateFlow = MutableStateFlow(MedTimerSettings(10 * 60, true, emptySet()))
+
         Mockito.`when`(
-            sharedPreferences.getBoolean(
-                PreferencesNames.WEEKEND_MODE, false
-            )
-        ).thenReturn(true)
-        Mockito.`when`(
-            sharedPreferences.getStringSet(
-                ArgumentMatchers.eq(PreferencesNames.WEEKEND_DAYS),
-                ArgumentMatchers.any<Set<String>>()
-            )
-        ).thenReturn(setOf<String>())
+            ReminderSchedulerUnitTest.preferencesDataSource.data
+        ).thenReturn(stateFlow)
 
         val medicineWithReminders1 =
             buildFullMedicine(1, ReminderSchedulerUnitTest.TEST_1)
@@ -82,29 +40,11 @@ internal class ReminderSchedulerWeekendModeTest {
         // 1.1.1970 is a Thursday
         val scheduler = scheduler
 
+        val stateFlow = MutableStateFlow(MedTimerSettings(10 * 60, true, setOf(DayOfWeek.SATURDAY.value.toString(), DayOfWeek.SUNDAY.value.toString())))
+
         Mockito.`when`(
-            sharedPreferences.getBoolean(
-                ArgumentMatchers.eq(
-                    PreferencesNames.WEEKEND_MODE
-                ), ArgumentMatchers.anyBoolean()
-            )
-        ).thenReturn(true)
-        Mockito.`when`(
-            sharedPreferences.getInt(
-                ArgumentMatchers.eq(
-                    PreferencesNames.WEEKEND_TIME
-                ), ArgumentMatchers.anyInt()
-            )
-        ).thenReturn(10 * 60)
-        val weekendDays = mutableSetOf<String>()
-        weekendDays.add(DayOfWeek.SATURDAY.value.toString())
-        weekendDays.add(DayOfWeek.SUNDAY.value.toString())
-        Mockito.`when`(
-            sharedPreferences.getStringSet(
-                ArgumentMatchers.eq(PreferencesNames.WEEKEND_DAYS),
-                ArgumentMatchers.any<Set<String>>()
-            )
-        ).thenReturn(weekendDays)
+            ReminderSchedulerUnitTest.preferencesDataSource.data
+        ).thenReturn(stateFlow)
 
         val medicineWithReminders1 =
             buildFullMedicine(1, ReminderSchedulerUnitTest.TEST_1)
