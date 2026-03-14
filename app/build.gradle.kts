14c4d4139ccfa08c195dbd0dbd50c5350db64082
Futsch1 <63090558+Futsch1@users.noreply.github.com>
2026-03-14 09:23:43 +0100

    Disable instrumentation test runner for now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,8 @@ android {
         @Suppress("DEPRECATION")
         resConfigs("en,ar,bg,cs,da,de,el,es,fi,fr,hu,it,iw,nl,pl,pt,ru,sv,ta,tr,uk,zh-rCN,zh-rTW")
 
-        // testInstrumentationRunner = "com.futsch1.medtimer.TestRunner"
+        //testInstrumentationRunner = "com.futsch1.medtimer.TestRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunnerArguments.putAll(
             mapOf(
                 "clearPackageData" to "true",
