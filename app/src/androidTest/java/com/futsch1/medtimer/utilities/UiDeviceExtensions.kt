2829ac1eb77d8ca59306848f726ea1236fa9681f
thomas kiljanczyk dev <thomas.kiljanczyk.dev@gmail.com>
2026-03-21 11:39:55 +0100

    #1285 add additional logs for test failure debugging purposes

--- app/src/androidTest/java/com/futsch1/medtimer/utilities/UiDeviceExtensions.kt
+++ app/src/androidTest/java/com/futsch1/medtimer/utilities/UiDeviceExtensions.kt
@@ -1,18 +1,26 @@
 package com.futsch1.medtimer.utilities
 
+import android.util.Log
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.BySelector
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
+
+const val TAG = "UiDeviceExtensions"
 
 fun UiDevice.waitForView(selector: BySelector, timeoutMs: Long) {
     wait(Until.hasObject(selector), timeoutMs)
 }
 
 fun UiDevice.closeNotification() {
+    // Swipe to close the notification shade
     swipe(displayWidth / 2, displayHeight, displayWidth / 2, displayHeight / 2, 20)
-    waitForIdle(200)
-    if (!findObjects(By.res("android:id/expand_button")).isEmpty() || !findObjects(By.descContains("Expand")).isEmpty()) {
+    waitForIdle(2000)
+
+    val isNotificationShadeOpen = findObjects(By.res("android:id/expand_button")).isNotEmpty() || findObjects(By.descContains("Expand")).isNotEmpty()
+    if (isNotificationShadeOpen) {
+        Log.w(TAG, "Expand button found after swipe, pressing back to dismiss")
         pressBack()
+        waitForIdle(2000)
     }
 }
