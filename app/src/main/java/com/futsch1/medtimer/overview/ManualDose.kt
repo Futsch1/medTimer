7f9a5c3662d7734115ce152afd7d5e0a138fb4dc
thomas kiljanczyk dev <thomas.kiljanczyk.dev@gmail.com>
2026-03-31 13:00:47 +0200

    #1285 fix test issues in ManualDoseTest.kt

--- app/src/main/java/com/futsch1/medtimer/overview/ManualDose.kt
+++ app/src/main/java/com/futsch1/medtimer/overview/ManualDose.kt
@@ -206,6 +206,8 @@ class ManualDose @AssistedInject constructor(
             }
         }
 
+        override fun toString(): String = name
+
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
             if (other == null || javaClass != other.javaClass) return false
