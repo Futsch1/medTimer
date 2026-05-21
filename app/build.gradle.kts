84111e1dd864ec6fa9c40e5c5867d669fa413a3c
Futsch1 <63090558+Futsch1@users.noreply.github.com>
2026-05-21 20:01:09 +0200

    Remove unused lifecycle-extensions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -129,12 +129,12 @@ android {
 
 dependencies {
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.lifecycle.service)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.preference.ktx)
-    implementation(libs.androidx.lifecycle.extensions)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.core.ktx)
