86bc140226cce38a33d62e59f97b761241a86335
Futsch1 <63090558+Futsch1@users.noreply.github.com>
2026-06-06 17:46:38 +0200

    Fix tests

--- feature/ui/src/main/java/com/futsch1/medtimer/feature/ui/overview/model/ScheduledReminderEvent.kt
+++ feature/ui/src/main/java/com/futsch1/medtimer/feature/ui/overview/model/ScheduledReminderEvent.kt
@@ -21,8 +21,10 @@ class ScheduledReminderEvent @AssistedInject constructor(
     }
 
     override val text: Spanned = reminderStringFormatter.formatScheduledReminder(scheduledReminder)
-    override val id: Int
-        get() = scheduledReminder.reminder.id + 1_000_000
+    override val id: Int = java.util.Objects.hash(
+        scheduledReminder.reminder.id,
+        scheduledReminder.timestamp.epochSecond
+    )
 
     override val timestamp: Long
         get() = scheduledReminder.timestamp.epochSecond
