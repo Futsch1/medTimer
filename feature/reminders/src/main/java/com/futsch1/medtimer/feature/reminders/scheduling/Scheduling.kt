6c2cc41bafa86f1c19c2ac3745ec190e630d7039
Futsch1 <63090558+Futsch1@users.noreply.github.com>
2026-06-07 16:33:58 +0200

    Optimize scheduling

--- feature/reminders/src/main/java/com/futsch1/medtimer/feature/reminders/scheduling/Scheduling.kt
+++ feature/reminders/src/main/java/com/futsch1/medtimer/feature/reminders/scheduling/Scheduling.kt
@@ -1,11 +1,11 @@
 package com.futsch1.medtimer.feature.reminders.scheduling
 
-import com.futsch1.medtimer.core.common.helpers.TimeHelper
 import com.futsch1.medtimer.core.domain.model.Reminder
 import com.futsch1.medtimer.core.domain.model.ReminderEvent
 import com.futsch1.medtimer.feature.reminders.TimeAccess
 import java.time.Instant
 import java.time.LocalDate
+import java.time.ZoneId
 
 fun interface Scheduling {
     fun getNextScheduledTime(): Instant?
@@ -55,8 +55,8 @@ abstract class SchedulingBase(
     }
 
     protected fun isOnDay(epochSeconds: Long, epochDay: Long): Boolean {
-        return TimeHelper.secondsSinceEpochToLocalDate(epochSeconds, systemZone)
-            .toEpochDay() == epochDay
+        val dayInterval = getDaySecondsStartEnd(epochDay, systemZone)
+        return dayInterval.first <= epochSeconds && epochSeconds < dayInterval.second
     }
 
     protected fun today(): Long {
@@ -81,6 +81,18 @@ abstract class SchedulingBase(
 
     private fun filterEvents(reminderEvents: List<ReminderEvent>): List<ReminderEvent> {
         return reminderEvents.filter { it.reminderId == reminder.id }
+    }
+
+    companion object {
+        val daySecondsStartEndCache: MutableMap<Long, Pair<Long, Long>> = mutableMapOf()
+
+        fun getDaySecondsStartEnd(epochDay: Long, zoneId: ZoneId): Pair<Long, Long> {
+            return daySecondsStartEndCache.getOrPut(epochDay) {
+                val start = LocalDate.ofEpochDay(epochDay).atStartOfDay(zoneId).toInstant()
+                val end = LocalDate.ofEpochDay(epochDay + 1).atStartOfDay(zoneId).toInstant()
+                Pair(start.epochSecond, end.epochSecond)
+            }
+        }
     }
 }
 
